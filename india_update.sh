3db19a2ec7eb1e0196eaa6b3e3e2ea9422bc9ad1
Mantanz <mantanz.care@gmail.com>
2020-04-01 03:31:05 +0530

    india data updated

---
+++ india_update.sh
@@ -0,0 +1,18 @@
+pwd=`pwd`
+echo $pwd
+cd ../api
+git pull
+sleep 60
+cd $pwd
+echo $pwd 
+
+git stash
+git pull --rebase
+git stash apply
+
+cp ./data_csv/india_data.json ./data_csv/india_data_yesterday.json
+cp ../api/data.json india_data.json
+
+git add -A
+git commit -m "india data updated" -a
+git push
