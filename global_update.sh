d8078dbe63b3d04add81c257fdd53a9ac64084e1
Mantanz <mantanz.care@gmail.com>
2020-04-02 23:25:45 +0530

    india data updated

--- global_update.sh
+++ global_update.sh
@@ -12,16 +12,15 @@ git stash apply
 
 python3 ./worldtotal.py ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv ./data_csv/cntry_timeseries_confirmed.csv
 
-python3 push.py cntry_timeseries_confirmed.csv
-
 python3 ./worldtotal.py ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv ./data_csv/cntry_timeseries_recovered.csv
 
-python3 push.py cntry_timeseries_recovered.csv
 
 python3 ./worldtotal.py ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv ./data_csv/cntry_timeseries_deaths.csv
-
-python3 push.py cntry_timeseries_deaths.csv
 
 git add -A
 git commit -m "global data updated" -a
 git push
+
+python3 push.py cntry_timeseries_confirmed.csv
+python3 push.py cntry_timeseries_recovered.csv
+python3 push.py cntry_timeseries_deaths.csv
--- india_update.sh
+++ india_update.sh
@@ -11,8 +11,11 @@ git pull --rebase
 git stash apply
 
 cp ./data_csv/india_data.json ./data_csv/india_data_yesterday.json
-cp ../api/data.json india_data.json
+cp ../api/data.json ./data_csv/india_data.json
 
 git add -A
 git commit -m "india data updated" -a
 git push
+
+python3 push.py india_data_yesterday.json
+python3 push.py india_data.json
