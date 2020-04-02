b73203e2271e5d3e1ff54fd685100e3a4dfa08e6
Mantanz <mantanz.care@gmail.com>
2020-04-03 00:00:52 +0530

    india data updated

--- india_update.sh
+++ india_update.sh
@@ -10,12 +10,20 @@ git stash
 git pull --rebase
 git stash apply
 
-cp ./data_csv/india_data.json ./data_csv/india_data_yesterday.json
-cp ../api/data.json ./data_csv/india_data.json
+diff --brief ../api/data.json ./data_csv/india_data.json >/dev/null
+comp_value=$?
+
+if [ $comp_value -eq 1 ]
+then
+    cp ./data_csv/india_data.json ./data_csv/india_data_yesterday.json
+    cp ../api/data.json ./data_csv/india_data.json
+    
+    python3 push.py india_data_yesterday.json
+    python3 push.py india_data.json
+else
+    echo "do something because they're identical"
+fi
 
 git add -A
 git commit -m "india data updated" -a
 git push
-
-python3 push.py india_data_yesterday.json
-python3 push.py india_data.json
